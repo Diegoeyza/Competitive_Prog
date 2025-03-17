630f217c4791ee7a27e13be8e82ede7b192edb36
Kylar <diegoeyzaguirreb@gmail.com>
2025-03-17 19:07:36 -0300

    Added the base code and a gitignore will modify the gitignore to upload the material of each class after the class ends

---
+++ class/1/steps.py
@@ -0,0 +1,9 @@
+from sys import stdin
+from itertools import *
+n= int(stdin.readline())
+
+for _ in range (n):
+    line= stdin.readline()
+    while j-i-2
+
+
---
+++ class/base.py
@@ -0,0 +1,16 @@
+from sys import stdin
+import io
+import math
+stdin = io.StringIO("""12
+10
+5
+0""")
+
+
+input_value = int(stdin.readline().strip())
+
+while input_value != 0:
+
+
+
+    input_value = int(stdin.readline().strip())
