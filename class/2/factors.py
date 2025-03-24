0cc9a242813c9f9f62e6f9bd8d4a863451c3ecf2
Diego <diegoeyzaguirreb@gmail.com>
2025-03-23 21:49:50 -0300

    Fixed the code, reduced complexity

--- class/2/factors.py
+++ class/2/factors.py
@@ -1,52 +1,33 @@
-from math import prod
+import math
+import itertools
 from sys import stdin
 import io
-def combinations(arr, r):
-    def bcktrack(start, path):
-        if len(path) == r:
-            result.append(tuple(path))
-            return
-        for i in range(start, len(arr)):
-            path.append(arr[i])
-            bcktrack(i + 1, path)
-            path.pop()
-    
-    result = []
-    bcktrack(0, [])
-    return result
 
 def get_factors(n):
-    factors = []
-    for i in range(2, n // 2 + 1):
+    factors = set()
+    for i in range(1, int(math.sqrt(n)) + 1):
         if n % i == 0:
-            factors.append(i)
-    return factors
+            factors.add(i)
+            factors.add(n // i)
+    return sorted(factors)
 
-def find_min_sum_factors(n):
-    factors = get_factors(n)
-    min_sum = float('inf')
-    if n==1:
+def min_sum_product(n):
+    if n == 1:
         return 2
 
-    for r in range(2, len(factors) + 1):
-        for combo in combinations(factors, r):
-            if prod(combo) == n:
-                print(combo)
-                min_sum = min(min_sum, sum(combo))
+    factors = get_factors(n)
+    min_sum = float("inf")
+    for size in range(2, min(5, len(factors) + 1)):  
+        for subset in itertools.combinations(factors, size):
+            if math.lcm(*subset) == n:
+                min_sum = min(min_sum, sum(subset))
 
-    return min_sum if min_sum != float('inf') else n+1
+    return min_sum if min_sum != float('inf') else n + 1  
 
-stdin = io.StringIO("""12
-10
-5
-9
-2147483647
-0""")
 
 input_value = int(stdin.readline().strip())
-case=1
+case = 1
 while input_value != 0:
-    print(f"Case {case}: {find_min_sum_factors(input_value)}")
-    case+=1
-
+    print(f"Case {case}: {min_sum_product(input_value)}")
+    case += 1
     input_value = int(stdin.readline().strip())
