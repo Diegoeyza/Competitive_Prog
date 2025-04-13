79c9f9ea65ea8b6807b95fb4c5f89e5890966cf4
Diego <diegoeyzaguirreb@gmail.com>
2025-04-13 00:37:58 -0400

    added a function to find the prime candidates

--- class/4(backtracking)/the_primes.py
+++ class/4(backtracking)/the_primes.py
@@ -4,21 +4,6 @@ import math
 stdin = io.StringIO("""11
 1""")
 
-def find_primes(limit,starter):
-    prim=set()
-    s_prim=set()
-    prim.add(2)
-    for i in range (2,limit):
-        p=True
-        for item in prim:
-            if i%item==0:
-                p=False
-        if (p):
-            prim.add(i)
-            if i>starter:
-                s_prim.add(i)
-    return(sorted(s_prim))
-
 def find_primes(limit, starter):
     prime = bytearray([True]) * (limit + 1)
     prime[0]=False
@@ -29,7 +14,21 @@ def find_primes(limit, starter):
             prime[num*num:limit+1:num] = bytearray([False]) * len(range(num*num, limit+1, num))
     return [i for i in range(starter + 1, limit + 1) if prime[i]]
 
+def find_candidates(primes,num_sum):
+    candidates = set()
+    for num in primes:
+        if sum(map(int, str(num))) == num_sum:
+            candidates.add(num)
+    return candidates
+
+
+
+
+
+
+
 total_sum = int(stdin.readline().strip())
 d=int(stdin.readline().strip())
-print(find_primes(99999,10000))
+primes=find_primes(99999,10000)
+candidates=find_candidates(primes,11)
 
