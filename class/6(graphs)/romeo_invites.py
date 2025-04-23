ea253368e9982917003f5b223de3624eb6dd72f2
Kylar <diegoeyzaguirreb@gmail.com>
2025-04-23 18:37:23 -0400

    finished the coloring graph as a base

---
+++ class/6(graphs)/romeo_invites.py
@@ -0,0 +1,77 @@
+from sys import stdin
+import io
+
+stdin = io.StringIO("""3
+
+5
+1 3
+1 1
+0
+1 5
+0
+
+8
+2 4 5
+2 1 3
+0
+0
+0
+1 3
+0
+1 5
+
+3
+2 2 3
+1 3
+1 1
+""")
+
+def kinda_red_black(graph, node, color, colors):
+    queue = [node]
+    colors[node] = color
+    count = [0, 0]
+    count[color] += 1
+
+    while len(queue)!=0:
+        u = queue.pop(0)
+        # print(u)\
+        for v in graph[u]:
+            if colors[v] == -1:
+                colors[v] = 1 - colors[u]
+                count[colors[v]] += 1
+                queue.append(v)
+            elif colors[v] == colors[u]:
+                return 0
+    # print(f"val= {count}")
+    return max(count)
+
+
+
+
+
+
+def calc_inv():
+    cases = int(stdin.readline())
+    stdin.readline()
+    for i in range(cases):
+        if i > 0:
+            stdin.readline()
+        n = int(stdin.readline())
+        graph = [[] for aux in range(n)]
+        for j in range(n):
+            data = [int(item) for item in stdin.readline().split()]
+            for enemy in data[1:]:
+                graph[j].append(enemy - 1)
+                graph[enemy - 1].append(j)
+
+        colors = [-1] * n
+        total = 0
+        for i in range(n):
+            if colors[i] == -1:
+                res = kinda_red_black(graph, i, 0, colors)
+                if res != 0:
+                    total += res
+        # print("total")
+        print(total)
+
+calc_inv()
