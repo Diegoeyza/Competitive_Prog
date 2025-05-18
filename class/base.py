0586ad0760a1233e9c0f94ca66b92075dda0706f
Diego <diegoeyzaguirreb@gmail.com>
2025-05-18 11:36:16 -0400

    Small modification to the base code for it to be more generic

--- class/base.py
+++ class/base.py
@@ -9,8 +9,9 @@ stdin = io.StringIO("""12
 
 input_value = int(stdin.readline().strip())
 
-while input_value != 0:
-
-
-
-    input_value = int(stdin.readline().strip())
+s, d = map(int, stdin.readline().split()) 
+while s!="":
+    try:
+        s, d = map(int, stdin.readline().split()) 
+    except:
+        s=""
